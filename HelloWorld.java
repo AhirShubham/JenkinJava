10749f4d9f1bd0f2cd042b07032211c82e7c8db0
AhirShubham <73291386+AhirShubham@users.noreply.github.com>
2022-08-24 14:46:55 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("How are you");
+	System.out.println("Long time no see");
+	
 }
 }
